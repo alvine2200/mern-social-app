843d645ba30980e7c3f4ed4bfc66f0954725f5ee
alvine2200 <alvinellavu@gmail.com>
2023-02-03 22:35:35 +0300

    post backend done

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -30,10 +30,9 @@ export const CreatePost = async (req, file) => {
 };
 export const getUserPosts = async (req, res) => {
   try {
-    const { id } = req.params.id;
-    const user = await User.findById({ _id: id });
-    const post = await Post.findOne({ userId: id });
-    return res.status(201).json({
+    const { id } = req.params.userId;
+    const post = await Post.find({ userId: id });
+    return res.status(200).json({
       status: "success",
       msg: "User Posts retrieved successfully",
       data: post,
@@ -46,7 +45,7 @@ export const getUserPosts = async (req, res) => {
 export const getFeedsPosts = async (req, res) => {
   try {
     const post = await Post.find();
-    return res.status(201).json({
+    return res.status(200).json({
       status: "success",
       msg: "feeds posts created successfully",
       data: post,
@@ -56,4 +55,30 @@ export const getFeedsPosts = async (req, res) => {
     return res.status(500).json({ status: "failed", error: error.message });
   }
 };
-export const likePost = async (req, res) => {};
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params.id;
+    const userId = req.user.userId;
+    const post = await Post.find({ _id: id });
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId);
+    }
+    const updatedPost = await Post.findByIdAndUpdate(
+      { _id: id },
+      { likes: post.likes },
+      { new: true }
+    );
+    return res.status(200).json({
+      status: "success",
+      msg: "Post liked/unliked successfully",
+      data: updatedPost,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ status: "failed", error: error.message });
+  }
+};
